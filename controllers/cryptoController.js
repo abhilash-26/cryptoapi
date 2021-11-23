b803e26499afe8ce800417df6daa6c3a5f6b3257
abhilash-26 <krabhilash226@gmail.com>
2021-11-23 15:52:35 +0530

    final changes

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -35,34 +35,39 @@ const getCurrencyPrice = async (req, res) => {
     //   });
     // }
 
+    // const result = await axios({
+    //   method: "GET",
+    //   url: "https://alpha-vantage.p.rapidapi.com/query",
+    //   params: {
+    //     interval: "5min",
+    //     function: "TIME_SERIES_INTRADAY",
+    //     // symbol: "MSFT",
+    //     symbol: "MSFT",
+    //     datatype: "json",
+    //     output_size: "compact",
+    //   },
+    //   headers: {
+    //     "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
+    //     "x-rapidapi-key": "d830d8ebdbmshfe5f70735a684ddp16b9b9jsn724f829a1b9b",
+    //   },
+    // });
+
     const result = await axios({
-      method: "GET",
-      url: "https://alpha-vantage.p.rapidapi.com/query",
-      params: {
-        interval: "5min",
-        function: "TIME_SERIES_INTRADAY",
-        // symbol: "MSFT",
-        symbol: "MSFT",
-        datatype: "json",
-        output_size: "compact",
-      },
-      headers: {
-        "x-rapidapi-host": "alpha-vantage.p.rapidapi.com",
-        "x-rapidapi-key": "d830d8ebdbmshfe5f70735a684ddp16b9b9jsn724f829a1b9b",
-      },
+      method: "get",
+      url: "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cbitcoin%2Cbinancecoin%2Cdogecoin%2Cchainlink%2Cvechain&vs_currencies=inr",
     });
 
     if (!result) {
       res.send({
         msg: "No result found",
         status: 1,
-        data: null,
+        Price: null,
       });
     } else {
       res.send({
         msg: "Data found is",
         status: 0,
-        data: result.data,
+        Price: result.data,
       });
     }
   } catch (error) {
